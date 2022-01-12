03c159c2a1b4ff1b306142d0834d471260174e43
Jakub Brych <jakub.brych@protonmail.com>
2022-01-12 13:42:44 +0100

    Initial commit

---
+++ src/com/javafee/poker/Card.java
@@ -0,0 +1,40 @@
+package com.javafee.poker;// add your own banner here
+
+public class Card implements Comparable<Card> {
+	private int suit; // use integers 1-4 to encode the suit
+	private int rank; // use integers 1-13 to encode the rank
+
+	public Card(int s, int v) {
+		//make a card with suit s and value v
+		this.suit = s;
+		this.rank = v;
+	}
+
+
+	public int getRank() {
+		return rank;
+	}
+
+	public int getSuit() {
+		return suit;
+	}
+
+	//@Override
+	/*public String toString() {
+		
+		return "com.javafee.poker.Card{" +
+				"suit=" + suit +
+				", rank=" + rank +
+				'}';
+	}*/
+	// add some more methods here if needed
+
+
+
+	@Override
+	public int compareTo(Card o) {
+		return 0;
+	}
+}
+
+
---
+++ src/com/javafee/poker/Player.java
@@ -0,0 +1,83 @@
+package com.javafee.poker;// add your own banner here
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Player {
+	private ArrayList<Card> hand; // the player's cards
+	private int bankroll; // bank tokenow
+	private int bet;
+	public Game game;
+
+	// you may choose to use more instance variables
+
+	public Player() {
+		// create a player here
+		int startTokens = ThreadLocalRandom.current().nextInt(10, 50);
+		bankroll += startTokens;
+		System.out.println("com.javafee.poker.Player tokens: " + startTokens);
+		hand = new ArrayList<>(5);
+		// zasilenie banku tokenow nowymi tokenami
+	}
+
+
+	public void addCard(Card c) {
+		// add the card c to the player's hand
+		// overloading is allowed
+		this.hand.add(c);
+	}
+
+	public void removeCard(Card c) {
+		// remove the card c from the player's hand
+		// overloading is allowed
+	}
+
+	public ArrayList<Card> getHand() {
+		// returns the hand
+		return hand;
+	}
+
+	public void winnings(int odds) {
+		// adjust bankroll if player wins
+		int win = odds*bet;
+		System.out.println("Your win: "+win);
+		setBankroll(win);
+		//game.play();
+
+
+	}
+
+	public int getBetsForGame() {
+		System.out.println("Your balance: "+bankroll);
+		System.out.println("How many tokens do u want to spent?");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Input bet:");
+		int bet = scanner.nextInt();
+		setBet(bet);
+		//System.out.println("Your bet: "+bet);
+		this.bankroll -= bet;
+		return bet;
+	}
+	public int getBankroll() {
+		// return current balance of bankroll
+
+		return 0;
+	}
+
+	public void setBankroll(int bankroll) {
+		// set the bankroll balance
+		this.bankroll+=bankroll;
+
+
+	}
+
+	public void setBet(int bet) {
+		this.bet = bet;
+	}
+
+	public int getBet() {
+		return bet;
+	}
+	// you may wish to use more methods here
+}
